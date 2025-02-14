fd6c9efd9ec16b84f584bc71b098306b5e3e0a96
Neil Wilson <neil@aldur.co.uk>
2025-02-14 12:40:51 +0000

    Initial quadlet configuration
    Use quadlet container units
    Run via user-level systemd
    Ensure login sessions linger

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,3 +3,8 @@
 # config/deploy.rb
 
 set :application, 'redmine'
+set :systemd_unit_dir, '.config/containers/systemd'
+set :systemd_local_path, 'config/quadlet'
+set :quadlet_container_name, "#{fetch(:application)}.container"
+set :quadlet_container_service_name, "#{fetch(:application)}.service"
+set :quadlet_container_file, File.join(fetch(:systemd_local_path), fetch(:quadlet_container_name))
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,8 @@
 
 # config/deploy/staging.rb
 
-server 'ipv6.srv-u5kwi.gb1s.brightbox.com', user: 'fedora', roles: %w[app]
+server 'ipv6.srv-q5d3t.gb1s.brightbox.com', user: 'ubuntu', roles: %w[app]
+server 'ipv6.srv-xch09.gb1s.brightbox.com', user: 'fedora', roles: %w[app]
 
 # MySQL Database Configuration (PARAMETERIZED)
 set :mysql_host,     ENV.fetch('MYSQL_HOST')
@@ -15,3 +16,8 @@ set :redmine_image,  'docker.io/library/redmine:latest'
 
 # Redmine Persistent Data Volume (important!)
 set :redmine_volume, 'redmine_data'
+
+# Rails cookie session data shared key
+# Generate with `bundle exec rake generate_secret_token` from a Rails installation
+set :secret_key_base, '9489b3eee4eccf317ed77407553e8adc97baca7c74dc7ee33cd93e4c8b69477'\
+                      'eea66eaedeb18af0be2679887c7c69c0a28c0fded0a71ea472a8c4laalal19cb'
